647a2460600269ba25d36b1503becb32074c80d7
yugo 0101 <yky.nakamura.010151@gmail.com>
2020-07-30 19:35:31 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <img class="container_background" src />
+    <article class="visual">
+      <img class src />
+    </article>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
